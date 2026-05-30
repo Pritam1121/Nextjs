71a174090326fa634ff281d3fb41e96f781b6ca1
Pritam Suryawanshi <pritamsuryawanshi1121@gmail.com>
2026-05-30 09:36:02 +0530

    feat: add error and loading components, update About and Home pages

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
  return(
-  <div>Hello World</div>
+    <>
+    <div>Hello World</div>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium rem temporibus autem obcaecati accusamus!</p>
+ </>
+  
  )
 }
